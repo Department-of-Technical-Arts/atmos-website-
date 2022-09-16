0bb798250e0af7d8bbd6afafb5812417490db7c1
gowtham <gowthambandlapalli@gmail.com>
2022-09-16 16:55:37 +0530

    updated-sponsors-page

---
+++ src/components/sponsors.js
@@ -0,0 +1,28 @@
+import React from 'react' ;
+import "../components/css/events.css"
+import NavBar from './Navbar/NavBar';
+
+function Sponsors() {
+
+    return(
+        <>
+            <div className='background-container'>
+                <div className='background'>
+                    <NavBar/>
+                    <div className='content'>
+                        <h1>SPONSORS</h1>
+                    
+                    </div>
+                    <div className='card-container'>
+                        <div className='hover-cards'><p>Competition</p></div>
+                        <div className='hover-cards'><p>workshops</p></div>
+                        <div className='hover-cards'><p>talks</p></div>
+                        <div className='hover-cards'><p>pro-shows</p></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Sponsors ;
