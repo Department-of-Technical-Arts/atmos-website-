b96e2ae0c8a3d1408393c34f25c50c59dffb04c1
gowtham <gowthambandlapalli@gmail.com>
2022-09-26 21:06:47 +0530

    final-changes-competition

--- src/components/competitions/competitions.jsx
+++ src/components/competitions/competitions.jsx
@@ -15,12 +15,14 @@ const Competitions = () => {
                     
                     </div>
                     <div className='card-container-competition-1 .competition-cards-1'>
-                      <a href="/competitions"><div className='hover-cards'><p>Competition</p></div></a>
-                      <a href="/workshops"><div className='hover-cards'><p>workshops</p></div></a>
+                      <p>Previous</p>
+                      <a href="/competitions"><div className='hover-cards'><p></p></div></a>
+                      <a href="/workshops"><div className='hover-cards'><p></p></div></a>
                     </div>
                     <div className='card-container-competition-2 .competition-cards-2'>
-                      <a href="/talks"><div className='hover-cards'><p>talks</p></div></a>
-                      <a href="/proshows"><div className='hover-cards'><p>pro-shows</p></div></a>
+                      <p>Current</p>
+                      <a href="/talks"><div className='hover-cards'><p></p></div></a>
+                      <a href="/proshows"><div className='hover-cards'><p></p></div></a>
                     </div>
                 </div>
             </div>
