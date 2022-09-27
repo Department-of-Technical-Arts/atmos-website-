e4e8219f5e5a3de482c9d7ce9f12769ae6963fa4
manaschubby <manaschubby@gmail.com>
2022-09-27 20:21:48 +0530

    Slide functionality added

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,102 +1,116 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import {React, useState} from 'react';
+import { Swiper, SwiperSlide, useSwiper, setSwiperInstance } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import placeholder from '../../images/competions/pawel-nolbert-62OK9xwVA0c-unsplash 1.png'
 import './slider.css'
 
+
 const Slider =  () => {
-    return (
-        // <div className="sliderContainer">
-            <>
-            <Swiper
-                spaceBetween={5}
-                grabCursor={true}
-                slidesPerView={2}
-                >
-                <SwiperSlide>
+        const [swiper, setSwiper] = useState(null);
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">SPES NOVO</div>
-                </SwiperSlide>
-                <SwiperSlide>
+        return (
+                // <div className="sliderContainer">
+                <>
+                <button className='swiperButton' onClick={() => swiper.slideNext()}>SLIDE</button>
+                <Swiper
+                        spaceBetween={5}
+                        grabCursor={true}
+                        slidesPerView={2}
+                        navigation
+                        onSwiper={(swiper) => setSwiper(swiper)}
+                        >
+                        
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">Under Pressure</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">SPES NOVO</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">CaseSensitive</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">Under Pressure</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">Nuclear Blues</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">CaseSensitive</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">GRAND PRIX MANIA</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">Nuclear Blues</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">ESCALADE</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">GRAND PRIX MANIA</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">BEAM IT</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">ESCALADE</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">CodeDash</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">BEAM IT</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">ProdStorm</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">CodeDash</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">FASTEST FRONTEND FIRST</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">ProdStorm</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">EXPERT ENCOUNTER</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">FASTEST FRONTEND FIRST</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">Sudostar</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">EXPERT ENCOUNTER</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">Techtainment</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">Sudostar</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">Line Follower</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">Techtainment</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">Cubing ATMOSphere</div>
-                </SwiperSlide>
-                <SwiperSlide>
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">Line Follower</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
 
-                        <img src={placeholder} alt="bruh" width="207px" height="244px" />
-                        <div className="placeholder-title">Wall Street Business Challenge</div>
-                </SwiperSlide>
-            </Swiper>
-            </>
-        // </div>
-    );
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">Cubing ATMOSphere</div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+
+                                <img src={placeholder} alt="bruh" width="207px" height="244px" />
+                                <div className="placeholder-title">Wall Street Business Challenge</div>
+                        </SwiperSlide>
+                        
+                </Swiper>
+
+                </>
+                // </div>
+        );
 };
+
+// const SwiperButtonNext = ({ children }) => {
+//         const swiper = useSwiper();
+//         return <button className='swiperButton' onClick={() => swiper.slideNext()}>{children}</button>;
+//       };
 export default Slider;
