afba9a7a1b1221746be71ee71405a3a7b0484eb9
Surya743 <gsuryashashank@gmail.com>
2022-09-29 18:02:11 +0530

    Adding Sponsors Page

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,26 +1,28 @@
 import React from 'react' ;
-import "../components/css/events.css"
-import NavBar from './Navbar/NavBar';
+
+
+import Imagegrid from './sponsors/imagegrid';
+import './sponsors/gallery.css'
 
 function Sponsors() {
 
     return(
         <>
-            <div className='background-container'>
-                <div className='background'>
-                    <NavBar/>
-                    <div className='content'>
-                        <h1>SPONSORS</h1>
-                    
-                    </div>
-                    <div className='card-container'>
-                        <div className='hover-cards'><p>Competition</p></div>
-                        <div className='hover-cards'><p>workshops</p></div>
-                        <div className='hover-cards'><p>talks</p></div>
-                        <div className='hover-cards'><p>pro-shows</p></div>
-                    </div>
-                </div>
-            </div>
+            <>
+      <div className="background-container-gallery">
+        <div className="background-gallery">
+          <div className="image-gallery"></div>
+          <div className="content-gallery">
+            <h1 className="oursponsors-title">Our Previous Sponsors</h1>
+            <h1 className='sponsors-title'>Title Sponsor</h1>
+            <h1 className='cotitle-sponsors'>Co-Title Sponsors</h1>
+            <h1 className='associate-sponsors'>Associate Sponsors</h1>
+            <h1 className='gold-sponsors'>Gold Sponsors</h1>
+          </div>
+          <Imagegrid />
+        </div>
+      </div>
+    </>
         </>
     )
 }
---
+++ src/components/sponsors/imagegrid.js
@@ -0,0 +1,156 @@
+import React from "react";
+import "./imagegrid.css";
+
+function Imagegrid() {
+  return (
+    <>
+     <div class="container-grid">
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+            <img src="/pepsicoLogo.png" alt="" className="logo-image" />
+          
+        </div>
+              
+      </div> 
+      
+           
+      
+    </div>
+
+    <div class="container-grid-2">
+    <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+       
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+        <img src="/ongcLogo.jpg" alt="" class="logo-image"/>
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+          <img src="/dukesLogo.png" alt="" class="logo-image"  />
+          
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+            <img src="/samsung.png" alt="" class="logo-image" />
+          
+        </div>
+              
+      </div> 
+      
+           
+      
+    </div>
+
+    <div class="container-grid-3">
+    <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+       
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+        <img src="/foreverDiary.png" alt="" class="forever-Logo"/>
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+          <img src="/jainIrrigation.png" alt="" class="logo-image"  />
+          
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+            <img src="/anushaProjects.png" alt="" class="logo-image" />
+          
+        </div>
+              
+      </div> 
+
+      
+      
+           
+      
+    </div>
+
+    <div class="container-grid-4">
+    <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+       
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+        <img src="/dltlabs.png" alt="" class="logo-image"/>
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+          <img src="/signetGroup.png" alt="" class="logo-image"  />
+          
+        </div>        
+      </div>
+       
+
+      
+      
+           
+      
+    </div>
+
+
+    <div class="container-grid-5">
+    <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+       
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+        <img src="/SBILogo.png" alt="" class="logo-image"/>
+        </div>        
+      </div>
+      <div class="gallery-container w-1 h-1">
+        <div class="gallery-item">
+          
+          <img src="/ITCLogo.png" alt="" class="logo-image"  />
+          
+        </div>        
+      </div>
+       
+
+      
+      
+           
+      
+    </div>
+    
+    </>
+   
+
+    
+    
+  );
+}
+
+export default Imagegrid;
