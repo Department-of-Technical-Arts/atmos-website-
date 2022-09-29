db6bd73cb9c9e27c681980251fca9bae87bcd0c1
Ishan Barpanda <ishanbarpanda@Ishans-Macbook-Air.local>
2022-09-29 18:21:43 +0530

    resolved gallery

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,30 +1,28 @@
-import React from 'react' ;
+import React from "react";
 
-
-import Imagegrid from './sponsors/imagegrid';
-import './sponsors/gallery.css'
+import Imagegrid from "./sponsors/imagegridSpons";
+import "./sponsors/spons.css";
 
 function Sponsors() {
-
-    return(
-        <>
-            <>
-      <div className="background-container-gallery">
-        <div className="background-gallery">
-          <div className="image-gallery"></div>
-          <div className="content-gallery">
-            <h1 className="oursponsors-title">Our Previous Sponsors</h1>
-            <h1 className='sponsors-title'>Title Sponsor</h1>
-            <h1 className='cotitle-sponsors'>Co-Title Sponsors</h1>
-            <h1 className='associate-sponsors'>Associate Sponsors</h1>
-            <h1 className='gold-sponsors'>Gold Sponsors</h1>
+  return (
+    <>
+      <>
+        <div className="background-container-gallery">
+          <div className="background-gallery">
+            <div className="image-gallery"></div>
+            <div className="content-gallery">
+              <h1 className="oursponsors-title">Our Previous Sponsors</h1>
+              <h1 className="sponsors-title">Title Sponsor</h1>
+              <h1 className="cotitle-sponsors">Co-Title Sponsors</h1>
+              <h1 className="associate-sponsors">Associate Sponsors</h1>
+              <h1 className="gold-sponsors">Gold Sponsors</h1>
+            </div>
+            <Imagegrid />
           </div>
-          <Imagegrid />
         </div>
-      </div>
+      </>
     </>
-        </>
-    )
+  );
 }
 
-export default Sponsors ;
+export default Sponsors;
---
+++ src/components/sponsors/imagegridSpons.js
@@ -0,0 +1,98 @@
+import React from "react";
+import "./imagegridSpons.css";
+
+function Imagegrid() {
+  return (
+    <>
+      <div class="container-grid-spons">
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item"></div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item"></div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/pepsicoLogo.png" alt="" className="logo-image" />
+          </div>
+        </div>
+      </div>
+
+      <div class="container-grid-2">
+        <div class="spons-container w-1 h-1">
+          <div class="sponsspons-item"></div>
+        </div>
+        <div class="sponsspons-container w-1 h-1">
+          <div class="sponsspons-item">
+            <img src="/ongcLogo.jpg" alt="" class="logo-image" />
+          </div>
+        </div>
+        <div class="sponsspons-container w-1 h-1">
+          <div class="sponsspons-item">
+            <img src="/dukesLogo.png" alt="" class="logo-image" />
+          </div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/samsung.png" alt="" class="logo-image" />
+          </div>
+        </div>
+      </div>
+
+      <div class="container-grid-3">
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item"></div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/foreverDiary.png" alt="" class="forever-Logo" />
+          </div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/jainIrrigation.png" alt="" class="logo-image" />
+          </div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/anushaProjects.png" alt="" class="logo-image" />
+          </div>
+        </div>
+      </div>
+
+      <div class="container-grid-4">
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item"></div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/dltlabs.png" alt="" class="logo-image" />
+          </div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/signetGroup.png" alt="" class="logo-image" />
+          </div>
+        </div>
+      </div>
+
+      <div class="container-grid-5">
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item"></div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/SBILogo.png" alt="" class="logo-image" />
+          </div>
+        </div>
+        <div class="spons-container w-1 h-1">
+          <div class="spons-item">
+            <img src="/ITCLogo.png" alt="" class="logo-image" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Imagegrid;
