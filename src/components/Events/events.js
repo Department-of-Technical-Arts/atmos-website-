e78153871a5c277cbdca2a9d6da730356cf41a8c
Rohan121002 <f20212739@hyderabad.bits-pilani.ac.in>
2022-10-02 15:43:44 +0530

    Events page made responsive

--- src/components/Events/events.js
+++ src/components/Events/events.js
@@ -13,16 +13,17 @@ function Events() {
                         <h1>EVENTS</h1>
                     
                     </div>
-                    <div className='card-container'>
-                        <a href="/competitions"><div className='hover-cards'><p>Competition</p></div></a>
-                        <a href="/workshops"><div className='hover-cards'><p>workshops</p></div></a>
-                        <a href="/talks"><div className='hover-cards'><p>talks</p></div></a>
-                        <a href="/proshows"><div className='hover-cards'><p>pro-shows</p></div></a>
+                    <div className='card-container ' >
+                       <a href="/competitions"><div className='hover-cards' ><p>Competition</p></div></a>
+                       <a href="/workshops"><div className='hover-cards'><p>workshops</p></div></a>
+                       <a href="/talks"> <div className='hover-cards'><p>talks</p></div></a>
+                       <a href="/proshows"><div className='hover-cards'> <p>pro-shows</p></div></a>
                     </div>
                 </div>
             </div>
         </>
     )
 }
+
 
 export default Events ;
