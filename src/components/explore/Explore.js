ca59dc5874b31c98e3e236f8f77091e8d4fb81e9
Ishan Barpanda <ishanbarpanda@Ishans-Macbook-Air.local>
2022-10-04 17:24:53 +0530

    explore page responsive

--- src/components/explore/Explore.js
+++ src/components/explore/Explore.js
@@ -1,62 +1,74 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import {} from "@mui/material";
-import "./Explore.css"
-import {IoIosArrowForward} from 'react-icons/io';
-import {Link,useNavigate} from "react-router-dom"
+import "./Explore.css";
+import { IoIosArrowForward } from "react-icons/io";
+import { Link, useNavigate } from "react-router-dom";
 
+function Explore() {
+  const navigate = useNavigate();
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+  const handleClick = async (e) => {
+    isClicked(true);
+    await delay(1000);
+    navigate("/events");
+  };
 
-function Explore(){
-
-    const navigate = useNavigate()
-    const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-    const handleClick = async(e) => {
-        isClicked(true)
-        await delay(1000)
-        navigate('/events')
-    }
-    
-    const [click,isClicked] = useState(false)
-    return(
+  const [click, isClicked] = useState(false);
+  return (
+    <>
+      {click ? (
         <>
-        {click ? <>
-        <div className='background-container1'>
-                <div className='bground1'>
-                    <div className="image1"></div>
-                    <div className='content1'>
-                        <h1 className="title1 small-title">TO EXPLORE</h1>
-                        <h1 className="title1 big-title">THE FUTURE</h1>
-                    </div>
-                    <div className="description1">
-                        <h4 className="desc1 white">Unforgettable experience to the future for 3 days!</h4>
-                        <h4 className="desc1 blue">Future is closer than you think</h4>
-                        <button className="explore-btn1" onClick={()=>{isClicked(true)}}>EXPLORE <IoIosArrowForward fontSize={18}/></button>
-                        
-                    </div>
-                    
-                </div>
+          <div className="background-container-explore1">
+            <div className="bground-explore1">
+              <div className="image1"></div>
+              <div className="content-explore1">
+                <h1 className="title-explore1 small-title">TO EXPLORE</h1>
+                <h1 className="title-explore1 big-title">THE FUTURE</h1>
+              </div>
+              <div className="description-explore1">
+                <h4 className="desc-explore1 white">
+                  Unforgettable experience to the future for 3 days!
+                </h4>
+                <h4 className="desc-explore1 blue">
+                  Future is closer than you think
+                </h4>
+                <button
+                  className="explore-btn1"
+                  onClick={() => {
+                    isClicked(true);
+                  }}
+                >
+                  EXPLORE <IoIosArrowForward className="ArrowForward" />
+                </button>
+              </div>
             </div>
-      </>
-      :
-        <div className='background-container'>
-                <div className='bground'>
-                    <div className="image"></div>
-                    <div className='content'>
-                        <h1 className="title small-title">TO EXPLORE</h1>
-                        <h1 className="title big-title">THE FUTURE</h1>
-                    </div>
-                    <div className="description">
-                        <h4 className="desc white">Unforgettable experience to the future for 3 days!</h4>
-                        <h4 className="desc blue">Future is closer than you think</h4>
-                        <button className="explore-btn" onClick={handleClick}>EXPLORE <IoIosArrowForward fontSize={18}/></button>
-                        
-                    </div>
-                    
-                </div>
-            </div>}
-            </>
-    )
+          </div>
+        </>
+      ) : (
+        <div className="background-container-explore">
+          <div className="bground-explore">
+            <div className="image-explore"></div>
+            <div className="content-explore">
+              <h1 className="title-explore small-title">TO EXPLORE</h1>
+              <h1 className="title-explore big-title">THE FUTURE</h1>
+            </div>
+            <div className="description-explore">
+              <h4 className="desc-explore white">
+                Unforgettable experience to the future for 3 days!
+              </h4>
+              <h4 className="desc-explore blue">
+                Future is closer than you think
+              </h4>
+              <button className="explore-btn" onClick={handleClick}>
+                EXPLORE <IoIosArrowForward className="ArrowForward" />
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Explore
+export default Explore;
