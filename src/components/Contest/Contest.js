9d3476ceaeed87573d125b65eecfdb6baaf4696e
manaschubby <manaschubby@gmail.com>
2022-10-04 19:38:56 +0530

    Contest pages mobile page updated

--- src/components/Contest/Contest.js
+++ src/components/Contest/Contest.js
@@ -1,28 +1,30 @@
 import {React, useState, useEffect} from 'react';
 import {  useParams, useSearchParams } from 'react-router-dom'
+import { useMediaQuery } from '@mui/material';
+
 import './Contest.css';
 
 
 function Contest({title = "CONTEST", desc = "Participants are invited to the battle of coding. Test your programming and algorithmic skills while competing with the world's best programmers solving the intense and innovative competition coding problems.", prize = "INR 10,000 PRIZE"}) 
 {
-    const params = useParams()
+    const isTablet = useMediaQuery('(max-width:480px)','(max-height:1024px)');
+    const isMobile = useMediaQuery('(max-width: 320px)','(max-height: 480px)');
 
+
+    const params = useParams()
     const competions = {
-        "SPES NOVO":"Earth is on the brink of apocalypse, you have been assigned a mission- SPES NOVO, a new hope for a new planet and a place to start life all over again. From exploration to survival, a lot can change, this space adventure by AD ASTRA succeeded in asserting it.",
-        "Under Pressure":"Teams of 2 participated in the fun filled particle simulation game 'Under Pressure' featuring Wordle. This event brought to us by Association of Chemical Engineers tested one's ability to handle situations under pressure in a really innovative way.",
-        "Nuclear Blues":"Participants of Nuclear Blues found themselves in the shoes of a company personnel hired to treat a river full of pollutants leaking from a mishap struck nuclear plant. This fun filled event brought an opportunity of showcasing basic math, bidding and trading skills for everyone",
+        "IDRL":"Earth is on the brink of apocalypse, you have been assigned a mission- SPES NOVO, a new hope for a new planet and a place to start life all over again. From exploration to survival, a lot can change, this space adventure by AD ASTRA succeeded in asserting it.",
+        "The Genesis":"Teams of 2 participated in the fun filled particle simulation game 'Under Pressure' featuring Wordle. This event brought to us by Association of Chemical Engineers tested one's ability to handle situations under pressure in a really innovative way.",
+        "ProdStorm":"Participants of Nuclear Blues found themselves in the shoes of a company personnel hired to treat a river full of pollutants leaking from a mishap struck nuclear plant. This fun filled event brought an opportunity of showcasing basic math, bidding and trading skills for everyone",
         "CaseSensitive":" A business case competition where participants get a chance to try their hands on a real-life market-related case. It is conducted in three rounds spread across a week with nationwide participation from undergraduate and post-graduate universities",
-        "GRAND PRIX MANIA":"The “Grand Prix Mania”, an event held by MEA of BPHC during Orbe Novo was a trivia contest based on Formula one. This quiz offered a perfect track for all the F1 enthusiasts to flaunt their expertise in all that is racing.",
-        "ESCALADE":"The IEEE of BPHC hosted &quot;Escalade,&quot; a hackathon for programmers and electronics enthusiasts, during Orbe Novo. The major goal of the competition was to assess participants aptitude for problem-solving in order to construct effective communication networks and algorithms. The participants in this event were motivated to innovate and come up with original solutions.",
-        "BEAM IT":"Popsicle sticks were utilised in “Beam It”, an event held by CEA of BPHC, to test the best of abilities offered by its participants. Popsicle sticks were used to construct bridges, and the winner was chosen based on how strong and durable the design was.",
-        "CodeDash":"The main coding competition held by ATMOS is called CodeDash. The objective of this online programming competition is to encourage participants to use their understanding of algorithms and data structures to solve problems. The main coding event of ATMOS is called CodeDash. It is an online programming competition focused on algorithms and data structures that encourages competitors to use their skills to solve problems.",
-        "ProdStorm":"To put an end to your summertime doldrums, PM Division@BHCG and Doremon Den are here. ProdStorm, our product case challenge at ATMOS, is a great way to learn about product management and develop your skills. To help you beat the dullness of the summer, PM Division@BHCG and Doremon Den are here. In ProdStorm, our product case challenge at ATMOS, you can learn more about product management and polish your talents.",
-        "FASTEST FRONTEND FIRST":"Are you into web development and design? Test your knowledge in our competition, Fastest Frontend First. This is a two-round event consisting of coding in CSS, HTML and JS. The first round consists of replicating an image provided using CSS and the second round requires the contestants to replicate an entire page",
-        "Sudostar":"Axiom surely challenged our wits and patience with this one. I am pretty sure you have heard of sudoku, these puzzles were based on sudoku but were surely different and far more interesting than the regular sudoku puzzles found in the newspapers.",
-        "Techtainment":"This showcase of intriguing projects by ARC definitely fuelled our innovative instincts. Students built 7 projects ranging from a Nerf Turret to a ‘Useless Box’ under the guidance of mentors from ARC. The ideas and concepts behind these projects were explained. Techtainment was an excellent opportunity to explore the domain of robotics and for those who are interested in the same.",
-        "Line Follower":"This competition organized by PHoEnix required the participants to build a line-follower bot and follow the track set in front of them. One’s experience and enthusiasm of using Raspberry Pi or Arduino were tested here. Their spatial skills were also put to test in order to navigate the course.",
-        "Cubing ATMOSphere":"It is a speedcubing competition with about 10 events like 3×3,4×4, blindfolded etc .It will be conducted in collaboration with World Cubing Association (WCA). Sponsor can be a online cubing store(like cubelelo) .",
-        "Wall Street Business Challenge":"A week-long business case study competition in which the participants are given real-life corporate problems and asked to present a solution in the form of slides. It has been the highest participation event of Orbe Novo with more than 800 registration. The prize money last time was 35K which we're expecting to increase to 45K to match with other competitions in academic circles"
+        "Courtroom":"The “Grand Prix Mania”, an event held by MEA of BPHC during Orbe Novo was a trivia contest based on Formula one. This quiz offered a perfect track for all the F1 enthusiasts to flaunt their expertise in all that is racing.",
+        "Mech Meverick":"The IEEE of BPHC hosted &quot;Escalade,&quot; a hackathon for programmers and electronics enthusiasts, during Orbe Novo. The major goal of the competition was to assess participants aptitude for problem-solving in order to construct effective communication networks and algorithms. The participants in this event were motivated to innovate and come up with original solutions.",
+        "Robowars":"Popsicle sticks were utilised in “Beam It”, an event held by CEA of BPHC, to test the best of abilities offered by its participants. Popsicle sticks were used to construct bridges, and the winner was chosen based on how strong and durable the design was.",
+        "Law Follower":"The main coding competition held by ATMOS is called CodeDash. The objective of this online programming competition is to encourage participants to use their understanding of algorithms and data structures to solve problems. The main coding event of ATMOS is called CodeDash. It is an online programming competition focused on algorithms and data structures that encourages competitors to use their skills to solve problems.",
+        "Anatomy Of Murder":"To put an end to your summertime doldrums, PM Division@BHCG and Doremon Den are here. ProdStorm, our product case challenge at ATMOS, is a great way to learn about product management and develop your skills. To help you beat the dullness of the summer, PM Division@BHCG and Doremon Den are here. In ProdStorm, our product case challenge at ATMOS, you can learn more about product management and polish your talents.",
+        "Wall Street Business Challenge":"Are you into web development and design? Test your knowledge in our competition, Fastest Frontend First. This is a two-round event consisting of coding in CSS, HTML and JS. The first round consists of replicating an image provided using CSS and the second round requires the contestants to replicate an entire page",
+        "Operation Zodiac":"Axiom surely challenged our wits and patience with this one. I am pretty sure you have heard of sudoku, these puzzles were based on sudoku but were surely different and far more interesting than the regular sudoku puzzles found in the newspapers.",
+        "Bid Up Vamps":"This showcase of intriguing projects by ARC definitely fuelled our innovative instincts. Students built 7 projects ranging from a Nerf Turret to a ‘Useless Box’ under the guidance of mentors from ARC. The ideas and concepts behind these projects were explained. Techtainment was an excellent opportunity to explore the domain of robotics and for those who are interested in the same.",
     }
     const workshops ={
         "Smart Automobile Workshop":"A 2-day online workshop on smart vehicles with certification was offered by Team Vulcan and SAE. This program was intended to teach students about industrial workflow and how electric automobile components work, as well as how concepts are developed, designs are created, and manufacturing processes are carried out.",
@@ -66,25 +68,16 @@ function Contest({title = "CONTEST", desc = "Participants are invited to the bat
             }
         })}
 
-        setReg((params.time=="curr"))
+        setReg(true)
     },[])
 
-    window.addEventListener('DOMContentLoaded', () => {
-        const title = document.getElementById("contest-page-title");
-    
-        console.log(title)
-        var numWords = title.text().split(' ').length; 
-    
-        if (numWords > 2) {
-            title.style.fontSize = "6.8px";
-        }
-    });
+
 
     return (
 
     <div className='background'>
         <div className='contest_image'></div>
-        <div className='content-left'>
+        <div className='content-left content-full'>
             <div className='photo'>
             </div>
             <div className='prize'>
@@ -103,7 +96,7 @@ function Contest({title = "CONTEST", desc = "Participants are invited to the bat
                 </div>
             </div>
         </div>
-        <div className='content-right'>
+        <div className='content-right content-full'>
             <div id="contest-page-title" className='contest-title'>
                 {/* {(params.type=="comp") && Object.keys(competions).map((value,i)=>{
                     console.log(competions)
--- src/components/competitions/competitions.jsx
+++ src/components/competitions/competitions.jsx
@@ -2,7 +2,7 @@ import React from 'react' ;
 import "./competitions.css";
 
 function Competitions() {
-
+    const competionsNames = ["IDRL","The Genesis","ProdStorm","CaseSensitive","Courtroom","Mech Meverick","Robowars","Law Follower","Anatomy Of Murder","Wall Street Business Challenge","Operation Zodiac","Bid Up Vamps"]
 
     return(
         <>
@@ -14,7 +14,14 @@ function Competitions() {
                     
                     </div>
                     <div className='card-container-competitions'>
-                        <div className='hover-cards-competitions'><p>IDRL</p></div>
+                        {
+                            Object.values(competionsNames).map((value,i)=>{
+                                return(
+                                    <a href={`/contest/comp/${i}`}><div className='hover-cards-competitions'><p>{value}</p></div></a>
+                                )
+                            })
+                        }
+                        {/* <div className='hover-cards-competitions'><p>IDRL</p></div>
                         <div className='hover-cards-competitions'><p>The Genesis</p></div>
                         <div className='hover-cards-competitions'><p>ProdStorm</p></div>
                         <div className='hover-cards-competitions'><p>Case Sensitive</p></div>
@@ -26,8 +33,7 @@ function Competitions() {
                         <div className='hover-cards-competitions'><p>Anatomy Of Murder</p></div>
                         <div className='hover-cards-competitions'><p>Wall Street Business Challenge</p></div>
                         <div className='hover-cards-competitions'><p>Operation Zodiac</p></div>
-                        <div className='hover-cards-competitions'><p>Bid Up Vamps</p></div>
-
+                        <div className='hover-cards-competitions'><p>Bid Up Vamps</p></div> */}
                     </div>
                 </div>
             </div>
