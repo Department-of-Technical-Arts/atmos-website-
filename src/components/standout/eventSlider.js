bb6f17ea055ca8617de6026c96ad7aed8226b525
manaschubby <manaschubby@gmail.com>
2022-10-05 15:24:18 +0530

    landing page photos added

--- src/components/standout/eventSlider.js
+++ src/components/standout/eventSlider.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./eventSlider.css";
+import competionsImages from '../../images/events-photos/competions-images';
+import talksImages from '../../images/events-photos/talks-images';
+import proshowsImages from '../../images/events-photos/proshows-images';
+import workshopsImages from '../../images/events-photos/workshops-images';
 
-export default function EventSlider() {
+export default function EventSlider({type}) {
+  const [images, setImage] = useState([])
+  useEffect(() => {
+    if (type=="comp"){
+      setImage(competionsImages)
+    }
+    else if (type=="talk"){
+      setImage(talksImages)
+    }
+    else if(type=="proshow"){
+      setImage(proshowsImages)
+    }
+    else if(type=="work"){
+      setImage(workshopsImages)
+    }
+  }, []);
   return (
     <div class="image-slider">
       <div class="image-slider-track">
-        <div class="slide-standout">
+        {Object.values(images).map((value,i)=>{
+          return(<div class="slide-standout" style={{ 
+            backgroundImage: `url(${images[i]})`
+          }}>
+          {/* <img style={{borderRadius:10}} src={value} /> */}
+        </div>)
+        })}
+        {/* <div class="slide-standout">
           <img style={{borderRadius:10}} src={require("./i1-min.jpeg")} />
         </div>
         <div class="slide-standout">
@@ -43,7 +69,7 @@ export default function EventSlider() {
         </div>
         <div class="slide-standout">
           <img style={{borderRadius:10}} src={require("./i1-min.jpeg")} />
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/components/standout/standout.js
+++ src/components/standout/standout.js
@@ -9,28 +9,28 @@ function Standout() {
         <div className="lec">
           <h1 className="standout-header">LECTURES</h1>
           <p className="standout-desc">
-            <EventSlider />
+            <EventSlider type={"proshow"}/>
           </p>
         </div>
 
         <div className="eve">
           <h1 className="standout-header">WORKSHOPS</h1>
           <p className="standout-desc">
-            <EventSlider />
+            <EventSlider type={"work"}/>
           </p>
         </div>
 
         <div className="talks">
           <h1 className="standout-header">TALKS</h1>
           <p className="standout-desc">
-            <EventSlider />
+            <EventSlider type={"talk"}/>
           </p>
         </div>
 
         <div className="comp">
           <h1 className="standout-header">COMPETITIONS</h1>
           <p className="standout-desc">
-            <EventSlider />
+            <EventSlider type={"comp"}/>
           </p>
         </div>
       </div>
