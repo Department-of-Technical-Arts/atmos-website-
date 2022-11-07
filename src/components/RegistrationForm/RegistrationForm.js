27ef201d7f2fbb59724a94cdea75184a8d8a5fe7
Sushobhit <sushobhitofficial@gmail.com>
2022-11-07 14:47:23 +0530

    Effective participation

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -5,9 +5,15 @@ import { firestore } from "../../config";
 import "./RegistrationForm.css";
 
 const RegistrationForm = () => {
-  const [form, setForm] = useState([]);
+  const [name, setName] = useState("");
+  const [contactNumber, setContactNumber] = useState(0)
+  const [college, setCollege] = useState ("")
+  const [year, setYear] = useState ("")
+  const [email, setEmail] = useState ("")
 
   const [numberOfParticipants, setParticipants] = useState (0)
+  const [currentParticipant, setCurrentParticipant] = useState(0)
+  const [allParticipants, setAllParticipantsData] = useState ([])
 
   const params = useParams();
   const { selectedEvent } = useSelector ((state) => state.displayData)
@@ -15,15 +21,13 @@ const RegistrationForm = () => {
     firestore
       .collection(params.id)
       .doc()
-      .set(form)
-      .then(() => console.log("Success"))
+      .set({allParticipants})
+      .then(() => alert("Success"))
       .catch((err) => console.log(err.message));
   };
 
   const setParticipantNumber = (event) => {
-    console.log(event.target.value);
     setParticipants(parseInt(event.target.value))
-    console.log(numberOfParticipants);
     const temporary = Array(parseInt(event.target.value)).fill ({
       name: "",
       email: "",
@@ -31,10 +35,52 @@ const RegistrationForm = () => {
       college: "",
       contactNumber: 0
     })
-    console.log(temporary);
-    setForm(temporary)
+    setAllParticipantsData(temporary)
   }
 
+  const onChange = (event) => {
+    const {value, name} = event.target
+    if (name === "name")
+      setName(value)
+    else if (name === "email")
+      setEmail(value)
+    else if (name === "contactNumber")
+      setContactNumber(value)
+    else if (name === "year")
+      setYear(value)
+    else
+      setCollege (value)
+  }
+
+  const onSaveHandler = () => {
+    let temporary = allParticipants
+    setCurrentParticipant(currentParticipant + 1)
+    let object = {
+      name: name,
+      year: year,
+      email: email,
+      college: college,
+      contactNumber: contactNumber
+    }
+    temporary[currentParticipant] = object
+    setAllParticipantsData(temporary)
+    setName(allParticipants[currentParticipant + 1].name)
+    setCollege(allParticipants[currentParticipant + 1].college)
+    setContactNumber(allParticipants[currentParticipant + 1].contactNumber)
+    setYear(allParticipants[currentParticipant + 1].year)
+    setEmail(allParticipants[currentParticipant + 1].email)
+  }
+
+  const onBackHandler = () => {
+    let temporary = allParticipants[currentParticipant - 1]
+    setName(temporary.name)
+    setCollege(temporary.college)
+    setEmail(temporary.email)
+    setContactNumber(temporary.contactNumber)
+    setYear(temporary.year)
+    setCurrentParticipant(currentParticipant - 1 )
+  }
+ 
   return (
     <div style={{ marginTop: "100px", color: "white" }}>
       <h3 className="register-title">REGISTRATION FORM</h3>
@@ -47,22 +93,28 @@ const RegistrationForm = () => {
           <option value={4}> 4 </option>
           <option value={5}> 5 </option>
         </select>
-        {form.map((eachParticipant, index) => {
-          return (
-            <div key={eachParticipant} className="register-form">
-              <h3>PARTICIPANT {`    ${index+1}`}</h3>
-              <input onClick={( ) => {}} value={eachParticipant.name} placeholder="Name" className="form-input" />
-              <input onClick={( ) => {}} value={eachParticipant.name} placeholder="Email" className="form-input" />
-              <input onClick={( ) => {}} value={eachParticipant.name} placeholder="Contact Number" className="form-input" />
-              <input onClick={( ) => {}} value={eachParticipant.name} placeholder="College" className="form-input" />
-              <input onClick={( ) => {}} value={eachParticipant.name} placeholder="Year" className="form-input" />
-            </div>
-          )
-        })}
-        
+        {numberOfParticipants !== 0 && <div className="register-form">
+            <h3>PARTICIPANT {currentParticipant + 1}</h3>
+            <input required onChange={onChange} value={name} name="name" placeholder="Name" className="form-input" type="text" />
+            <input required onChange={onChange} name="email" value={email} placeholder="Email" className="form-input" type="email" />
+            <input required onChange={onChange} name="contactNumber" value={contactNumber} placeholder="Contact Number" className="form-input" />
+            <input required onChange={onChange} name="college" value={college} placeholder="College" className="form-input" type="text" />
+            <input required onChange={onChange} name="year" value={year} placeholder="Year" className="form-input" />
+          </div>}
+        {currentParticipant > 0 && <button onClick={onBackHandler} className="form-btn">
+          BACK
+        </button>}
+        {(currentParticipant + 1) === numberOfParticipants ? 
         <button onClick={submitHandler} className="form-btn">
           SUBMIT
-        </button>
+        </button> : <div> 
+          {(numberOfParticipants !== 0) && 
+          <button onClick={onSaveHandler} className="form-btn">
+            NEXT
+          </button>
+          }
+        </div>
+        }
       </div>
     </div>
   );
