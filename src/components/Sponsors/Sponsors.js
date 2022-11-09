eb47e7c814c14e403228958feb75716460b56216
Surya743 <gsuryashashank@gmail.com>
2022-11-09 19:15:37 +0530

    Changing the directory for sponsors images to assets

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -15,7 +15,7 @@ const Imagegrid = () => {
     <div class="container-grid-1-item">      
       <div class="spons-container w-6 h-1">
         <div class="spons-item">
-          <img src="/pepsicoLogo.png" alt="" className="pepsico-image" />
+          <img src={require("../../assets/pepsicoLogo.png")} alt="" className="pepsico-image" />
         </div>
       </div>
     </div> 
@@ -25,17 +25,17 @@ const Imagegrid = () => {
     <div class="container-grid-spons">  
         <div class="spons-container w-1 h-1">
           <div class="sponsspons-item">
-            <img src="/ongcLogo.jpg" alt="" class="logo-image" />
+            <img src={require("../../assets/ongcLogo.jpg")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="sponsspons-item">
-            <img src="/dukesLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/dukesLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/samsung.png" alt="" class="logo-image" />
+            <img src={require("../../assets/samsung.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
@@ -51,17 +51,17 @@ const Imagegrid = () => {
         
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/foreverDiary.png" alt="" class="forever-logo" />
+            <img src={require("../../assets/foreverDiary.png")} alt="" class="forever-logo" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/jainIrrigation.png" alt="" class="logo-image" />
+            <img src={require("../../assets/jainIrrigation.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/anushaProjects.png" alt="" class="logo-image" />
+            <img src={require("../../assets/anushaProjects.png")} alt="" class="logo-image" />
           </div>
         </div>
       </div>
@@ -72,12 +72,12 @@ const Imagegrid = () => {
        
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/dltlabs.png" alt="" class="logo-image" />
+            <img src={require("../../assets/dltlabs.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/signetGroup.png" alt="" class="logo-image" />
+            <img src={require("../../assets/signetGroup.png")} alt="" class="logo-image" />
           </div>
         </div>
       </div>
@@ -91,17 +91,17 @@ const Imagegrid = () => {
         
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/SBILogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/SBILogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/ITCLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/ITCLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/mahindraEPGLogo.jpg" alt="" class="logo-image" />
+            <img src={require("../../assets/mahindraEPGLogo.jpg")} alt="" class="logo-image" />
           </div>
         </div>
       </div>
@@ -110,17 +110,17 @@ const Imagegrid = () => {
         
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/kothariGroupLogo.jpg" alt="" class="logo-image" />
+            <img src={require("../../assets/kothariGroupLogo.jpg")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/NTPCLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/NTPCLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/asraLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/asraLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         
@@ -129,7 +129,7 @@ const Imagegrid = () => {
       <div class="container-grid-1-item">
       <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/redBullLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/redBullLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         
@@ -144,17 +144,17 @@ const Imagegrid = () => {
      
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/peepulLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/peepulLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/elitebiotechLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/elitebiotechLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/unacademyLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/unacademyLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
       </div>
@@ -163,17 +163,17 @@ const Imagegrid = () => {
      
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/PGOLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/PGOLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/museeMusicalLogo.jpg" alt="" class="logo-image" />
+            <img src={require("../../assets/museeMusicalLogo.jpg")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/GMSolarLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/GMSolarLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
       </div>
@@ -182,17 +182,17 @@ const Imagegrid = () => {
       
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="GatsbyLogo-2.png" alt="" class="logo-image" />
+            <img src={require("../../assets/GatsbyLogo-2.png")} alt="" class="logo-image" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/adda52logo.jpeg" alt="" class="adda-logo" />
+            <img src={require("../../assets/adda52logo.jpeg")} alt="" class="adda-logo" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/greenSoleLogo.jpg" alt="" class="logo-image" />
+            <img src={require("../../assets/greenSoleLogo.jpg")} alt="" class="logo-image" />
           </div>
         </div>
       </div>
@@ -201,12 +201,12 @@ const Imagegrid = () => {
      
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/redFMLogo.webp" alt="" class="redfm-logo" />
+            <img src={require("../../assets/redFMLogo.webp")} alt="" class="redfm-logo" />
           </div>
         </div>
         <div class="spons-container w-1 h-1">
           <div class="spons-item">
-            <img src="/stucredLogo.png" alt="" class="logo-image" />
+            <img src={require("../../assets/stucredLogo.png")} alt="" class="logo-image" />
           </div>
         </div>
       </div>
