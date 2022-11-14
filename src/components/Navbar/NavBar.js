2ede4a7aa02d4d209049aac37d573846c3c67b5e
Ishan Barpanda <ishanbarpanda@Ishans-Macbook-Air.local>
2022-11-14 13:27:43 +0530

    navbar background updated

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,34 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DrawerComp from "../Drawer/Drawer";
-import {AppBar, Toolbar, useMediaQuery} from "@mui/material";
-import {Link} from "react-router-dom";
-import "./NavBar.css"
-import mainLogo from "../../images/Asset2.png"
-
-
+import { AppBar, Toolbar, useMediaQuery } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+import mainLogo from "../../images/Asset2.png";
 
 const NavBar = () => {
-  const isTablet = useMediaQuery('(max-width:480px)','(max-height:1024px)');
-  const isMobile = useMediaQuery('(max-width: 320px)','(max-height: 480px)');
+  const isTablet = useMediaQuery("(max-width:480px)", "(max-height:1024px)");
+  const isMobile = useMediaQuery("(max-width: 320px)", "(max-height: 480px)");
   let paddingTop;
-  if(isTablet){
+  if (isTablet) {
     paddingTop = "4rem";
-  }
-  else{
+  } else {
     paddingTop = 0;
   }
+
   return (
     <div className="appbar">
-    <AppBar className="appbar" sx={{ bgcolor: "transparent" }} elevation={0}>
-      <Toolbar>
-        <a href="/" className="wrapper">
-          <img src={mainLogo} className="logoimg"></img>
-        </a>
-      <DrawerComp/>
-      </Toolbar>
-    </AppBar>
+      <AppBar className="appbar" sx={{ bgcolor: "transparent" }} elevation={0}>
+        <Toolbar>
+          <a href="/" className="wrapper">
+            <img src={mainLogo} className="logoimg"></img>
+          </a>
+          <DrawerComp />
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
 
-export default NavBar; 
+export default NavBar;
