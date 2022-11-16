96a66c89eea1a8cf1814552c18d0c0958ca2186c
manaschubby <manaschubby@gmail.com>
2022-11-16 20:30:55 +0530

    Metadata Desc updated

--- src/components/Standout/EventSlider.js
+++ src/components/Standout/EventSlider.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { urlEndpoint } from "../../config";
 
 const EventSlider = ({ type, data }) => {
- 
+
   const navigate = useNavigate();
 
   function imageClicked() {
